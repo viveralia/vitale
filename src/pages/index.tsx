47cbf6dd675478f507848937c5958ecd96d5fbeb
paulinevalero <paulinevalero@outlook.com>
2021-03-09 16:54:35 -0600

    feat: 🎸 Initial configuration theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
+import Button from "@material-ui/core/Button";
 import React, { FC } from "react";
 
+import Layout from "../components/layout";
+
 const IndexPage: FC = () => {
-  return <p>Home</p>;
+  return (
+    <Layout>
+      <p color="textSecondary">Home page</p>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </Layout>
+  );
 };
 
 export default IndexPage;
