2468386eb19f745178e922c83458b964bb9b20f4
paulinevalero <paulinevalero@outlook.com>
2021-03-10 16:41:47 -0600

    feat: 🎸 Add h2-h6 weight to 700

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -21,17 +21,30 @@ const theme = createMuiTheme({
     h1: {
       fontWeight: 700,
     },
+    h2: {
+      fontWeight: 700,
+    },
+    h3: {
+      fontWeight: 700,
+    },
+    h4: {
+      fontWeight: 700,
+    },
+    h5: {
+      fontWeight: 700,
+    },
+    h6: {
+      fontWeight: 700,
+    },
   },
 });
 
 const Layout: FC = ({ children }) => {
   return (
-    <React.Fragment>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        {children}
-      </ThemeProvider>
-    </React.Fragment>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
   );
 };
 
